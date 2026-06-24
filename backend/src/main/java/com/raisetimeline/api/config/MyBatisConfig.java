2750589a3252ef4eff8c05863305e5f1f2e7d526
yamamotonaoki3 <3219bad@gmail.com>
2026-06-24 20:53:42 +0900

    fix: mybatis-spring-boot-starter を mybatis-spring:4.0.0 に置き換えて Spring Boot 4.x に対応

---
+++ backend/src/main/java/com/raisetimeline/api/config/MyBatisConfig.java
@@ -0,0 +1,26 @@
+package com.raisetimeline.api.config;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages = "com.raisetimeline.api", annotationClass = Mapper.class)
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+
+        org.apache.ibatis.session.Configuration config = new org.apache.ibatis.session.Configuration();
+        config.setMapUnderscoreToCamelCase(true);
+        factoryBean.setConfiguration(config);
+
+        return factoryBean.getObject();
+    }
+}
